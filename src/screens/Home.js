2bb7c31e2fde01a9e4701d934ee72132013a85ad
911harm <911harm@gmail.com>
2020-12-26 12:56:40 -0500

    carousel s

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import CarouselCustom  from '../components/CarouselCustom'
 import Items from '../components/Items'
 
 export default function Home() {
     return (
         <React.Fragment>
+            <CarouselCustom></CarouselCustom>
             <Items></Items>
         </React.Fragment>
     )
